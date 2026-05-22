81cb5c552733cae0bbbdc7016b95210ca01bfbb4
Mahardika <herolone83@gmail.com>
2026-05-22 18:50:34 +0700

    Update Code_with_LSP.py

--- Code_with_LSP.py
+++ Code_with_LSP.py
@@ -1,1 +1,13 @@
+class Hewan(self):
+  def __init__(self, nama, jenis):
+    self.nama = nama
+    self.jenis = jenis
 
+  def makan(self):
+    print(f"Hewan {self.nama} sedang makan.")
+
+class HewawTerbang(Hewan):
+  def __init__(self):
+    print(f"Hewan {self.nama} bisa terbang.")
+
+#BELUM SELESAI!
