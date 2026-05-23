7b16736d6569386558585d71d862fe8de1894f79
Mahardika <herolone83@gmail.com>
2026-05-23 08:56:46 +0700

    Update Code_with_LSP.py

--- Code_with_LSP.py
+++ Code_with_LSP.py
@@ -10,4 +10,39 @@ class HewawTerbang(Hewan):
   def __init__(self):
     print(f"Hewan {self.nama} bisa terbang.")
 
-#BELUM SELESAI!
+class Burung(HewanTerbang):
+  pass
+
+class Gajah(Hewan):
+  pass 
+
+class Kandang:
+  def __init__(self):
+    self.hewan_list = []
+
+  def tambah_hewan(self):
+    self.hewan_list.append(hewan)
+
+  def bersihkan_kandang(self):
+    print("Kandang dibersihkan")
+
+class KebunBinatang(self):
+  def __init__(self):
+    self.kandang = kandang()
+
+  def rawat_semua_hewan(self):
+    for hewan in self.kandang.hewan_list:
+      hewan.makan()
+
+        if isistance(hewan, hewan_terbang):
+          hewan.terbang()
+
+burung = Burung(perkutut, burung)
+sapi = Sapi(sapi, mamalia)
+
+zoo = KebunBinatang()
+
+zoo.kandang.append(burung)
+zoo.kandang.append(sapi)
+
+zoo.rawat_semua_hewan()
