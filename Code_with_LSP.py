0bd424bd0b2cec9e5aadaacf1b8e13116d2f9077
Mahardika <herolone83@gmail.com>
2026-05-23 09:09:26 +0700

    Update Code_with_LSP.py

--- Code_with_LSP.py
+++ Code_with_LSP.py
@@ -1,4 +1,4 @@
-class Hewan(self):
+class Hewan:
   def __init__(self, nama, jenis):
     self.nama = nama
     self.jenis = jenis
@@ -6,43 +6,43 @@ class Hewan(self):
   def makan(self):
     print(f"Hewan {self.nama} sedang makan.")
 
-class HewawTerbang(Hewan):
-  def __init__(self):
+class HewanTerbang(Hewan):
+  def terbang(self):
     print(f"Hewan {self.nama} bisa terbang.")
 
 class Burung(HewanTerbang):
   pass
 
-class Gajah(Hewan):
+class Sapi(Hewan):
   pass 
 
 class Kandang:
   def __init__(self):
     self.hewan_list = []
 
-  def tambah_hewan(self):
+  def tambah_hewan(self, hewan):
     self.hewan_list.append(hewan)
 
   def bersihkan_kandang(self):
     print("Kandang dibersihkan")
 
-class KebunBinatang(self):
+class KebunBinatang:
   def __init__(self):
-    self.kandang = kandang()
+    self.kandang = Kandang()
 
   def rawat_semua_hewan(self):
     for hewan in self.kandang.hewan_list:
       hewan.makan()
+      
+      if isinstance(hewan, HewanTerbang):
+            hewan.terbang()
 
-        if isistance(hewan, hewan_terbang):
-          hewan.terbang()
-
-burung = Burung(perkutut, burung)
-sapi = Sapi(sapi, mamalia)
+burung = Burung("Perkutut", "Burung")
+sapi = Sapi("sapi", "mamalia")
 
 zoo = KebunBinatang()
 
-zoo.kandang.append(burung)
-zoo.kandang.append(sapi)
+zoo.kandang.tambah_hewan(burung)
+zoo.kandang.tambah_hewan(sapi)
 
 zoo.rawat_semua_hewan()
