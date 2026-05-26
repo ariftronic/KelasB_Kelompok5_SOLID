883931a9314a07033f1e30f42fb7b3f9bd63319b
Dravian <danangraflijuvianto@student.uns.ac.id>
2026-05-26 08:46:45 +0700

    Update Dependency Inversion Principle (DIP).py

--- solusi/Dependency Inversion Principle (DIP).py
+++ solusi/Dependency Inversion Principle (DIP).py
@@ -1,1 +1,8 @@
+class KebunBinatang:
+    # Menerima object kandang dari luar (Dependency Injection)
+    def _init_(self, kandang):
+        self.kandang = kandang
 
+    def rawat_semua_hewan(self):
+        for hewan in self.kandang.hewan_list:
+            hewan.makan()
