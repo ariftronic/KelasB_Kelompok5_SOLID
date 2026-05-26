d9fc8156ee5ada2a0b90965e2b4dd34ff9f11e31
Dravian <danangraflijuvianto@student.uns.ac.id>
2026-05-26 08:45:57 +0700

    Update Interface Segregation Principle (ISP).py

--- solusi/Interface Segregation Principle (ISP).py
+++ solusi/Interface Segregation Principle (ISP).py
@@ -1,1 +1,9 @@
+class BisaTerbang(ABC):
+    @abstractmethod
+    def terbang(self): pass
 
+class Burung(Hewan, BisaTerbang):
+    def makan(self):
+        print(f"{self.nama} makan biji-bijian.")
+    def terbang(self):
+        print(f"{self.nama} sedang terbang tinggi.")
