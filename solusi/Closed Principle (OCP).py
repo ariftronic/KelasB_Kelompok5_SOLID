0f3125a622c76efcfaf327a05a279103ef8e6d0e
Dravian <danangraflijuvianto@student.uns.ac.id>
2026-05-26 08:45:04 +0700

    Update Closed Principle (OCP).py

--- solusi/Closed Principle (OCP).py
+++ solusi/Closed Principle (OCP).py
@@ -1,1 +1,13 @@
+from abc import ABC, abstractmethod
 
+class Hewan(ABC):
+    def _init_(self, nama):
+        self.nama = nama
+    
+    @abstractmethod
+    def makan(self):
+        pass
+
+class Kucing(Hewan):
+    def makan(self):
+        print(f"{self.nama} sedang makan ikan.")
